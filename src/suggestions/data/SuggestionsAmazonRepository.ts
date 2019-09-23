658fdb44f9284b14ef5eed53a7a15b3e0d1f68ba
Jorge Sánchez <xurxosf@gmail.com>
2019-09-23 14:33:00 +0200

    Map alias to search index to retrieve scope for suggestions

--- src/suggestions/data/SuggestionsAmazonRepository.ts
+++ src/suggestions/data/SuggestionsAmazonRepository.ts
@@ -3,6 +3,127 @@ import SuggestionRepository from "../domain/Boundaries";
 import { Suggestion, SuggestionCategory } from "../domain/Suggestion";
 
 export default class SuggestionsAmazonRepository implements SuggestionRepository {
+    public categories = [
+        {
+            alias: "grocery",
+            searchIndex: "Grocery"
+        },
+        {
+            alias: "mobile-apps",
+            searchIndex: "MobileApps"
+        }, {
+            alias: "baby",
+            searchIndex: "Baby"
+        },
+        {
+            alias: "beauty",
+            searchIndex: "Beauty"
+        },
+        {
+            alias: "diy",
+            searchIndex: "Tools"
+        },
+        {
+            alias: "gift-cards",
+            searchIndex: "GiftCards"
+        },
+        {
+            alias: "automotive",
+            searchIndex: "Automotive"
+        },
+        {
+            alias: "sporting",
+            searchIndex: "SportingGoods"
+        },
+        {
+            alias: "electronics",
+            searchIndex: "Electronics"
+        },
+        {
+            alias: "luggage",
+            searchIndex: "Luggage"
+        },
+        {
+            alias: "handmade",
+            searchIndex: "Handmade"
+        },
+        {
+            alias: "kitchen",
+            searchIndex: "Kitchen"
+        },
+        {
+            alias: "lighting",
+            searchIndex: "Lighting"
+        },
+        {
+            alias: "industrial",
+            searchIndex: "Industrial"
+        },
+        {
+            alias: "computers",
+            searchIndex: "PCHardware"
+        },
+        {
+            alias: "mi",
+            searchIndex: "MusicalInstruments"
+        },
+        {
+            alias: "lawngarden",
+            searchIndex: "LawnAndGarden"
+        },
+        {
+            alias: "jewelry",
+            searchIndex: "Jewelry"
+        },
+        {
+            alias: "toys",
+            searchIndex: "Toys"
+        },
+        {
+            alias: "stripbooks",
+            searchIndex: "Books"
+        },
+        {
+            alias: "digital-music",
+            searchIndex: "MP3Downloads"
+        },
+        {
+            alias: "office-products",
+            searchIndex: "OfficeProducts"
+        },
+        {
+            alias: "dvd",
+            searchIndex: "DVD",
+        },
+        {
+            alias: "watches",
+            searchIndex: "Watches"
+        },
+        {
+            alias: "apparel",
+            searchIndex: "Apparel"
+        },
+        {
+            alias: "hpc",
+            searchIndex: "HealthPersonalCare"
+        },
+        {
+            alias: "software",
+            searchIndex: "Software"
+        },
+        {
+            alias: "digital-text",
+            searchIndex: "KindleStore"
+        },
+        {
+            alias: "videogames",
+            searchIndex: "VideoGames"
+        },
+        {
+            alias: "shoes",
+            searchIndex: "Shoes"
+        }
+    ];
 
     public get(prefix: string): Promise<Suggestion[]> {
         return new Promise((resolve, reject) => {
@@ -10,11 +131,20 @@ export default class SuggestionsAmazonRepository implements SuggestionRepository
                 .then((response) => response.json())
                 .then((resJson: any) => {
                     const suggestions = resJson.suggestions.map((sug: any) => {
+                        let scopes = [];
+
+                        if (sug.scopes) {
+                            scopes = scopes.filter((sco) => this.categories.find((cat) => cat.alias === sco.alias));
+                        }
+
                         return {
                             categories: sug.scopes ? sug.scopes.map((scope: any) => {
+
+                                const searchIndex = this.categories.find((c) => c.alias === scope.value).searchIndex;
+
                                 return {
                                     display: scope.display,
-                                    value: scope.value
+                                    value: searchIndex
                                 };
                             }) : [],
                             value: sug.value
